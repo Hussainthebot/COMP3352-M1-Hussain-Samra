5381e3d7708d0d6d52f2f2f529cd6b31bfb0602d
Mehboob Hussain <hussain.hussain@du.edu>
2026-02-13 14:38:15 -0700

    removed some comments

--- Env.hs
+++ Env.hs
@@ -9,8 +9,6 @@ import qualified Data.Map as M
 
 -- =====================================================
 -- ENVIRONMENT (SYMBOL TABLE)
-
--- hi
 -- =====================================================
 -- This module is used by COMPILER PASSES.
 -- It maps names to metadata (usually other names).
